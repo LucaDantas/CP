ecd0551d35da5f32c4f68e09f15b02b14717e680
LucaDantas <luca100sdantas2@gmail.com>
2019-12-20 21:14:39 -0300

    implemented this other solution but there still is something wrong which I need to solve, but for the next time

--- codeforces/77edu/game_with_traps.cpp
+++ codeforces/77edu/game_with_traps.cpp
@@ -9,18 +9,47 @@ vector<iii> traps(200010);
 vector<int> soldiers(200010);
 int m, n, k, t;
 
-bool path(int mx, int minag){
-  int last = 0;
-  for(int i = 0; i<k; i++){
-    mx -= (traps[i].first.first - last);
-    last = traps[i].first.first;
-    if(traps[i].second > minag) mx -= traps[i].first.second;
-    if(mx < 0) return false;
+// bool path(int mx, int minag){
+//   int last = 0;
+//   for(int i = 0; i<k; i++){
+//     mx -= (traps[i].first.first - last);
+//     last = traps[i].first.first;
+//     if(traps[i].second > minag) mx -= traps[i].first.second;
+//     if(mx < 0) return false;
+//   }
+//   return true;
+// }
+
+int sorttraps(iii a, iii b){
+  if(a.first.first > b.first.first) return 1;
+  else return 0;
+}
+
+bool path(int price, int minag){
+  int current = 0, i = 0, end;
+  while(i < k){
+    if(traps[i].second > minag){
+      price -= traps[i].first.first - current - 1;
+      current = traps[i].first.first - 1;
+      end = traps[i].first.second;
+      i++;
+      while(i < k && traps[i].first.first <= end){
+        if(traps[i].second > minag){
+          end = max(end,traps[i].first.second);
+        }
+        i++;
+      }
+      price -= 2*(end-current);
+    }
+    else i++;
   }
-  return true;
+  price -= n - current;
+  if(price >= 0) return true;
+  else return false;
 }
 
 int bsa(){
+  sort(traps.begin(), traps.begin() + k, sorttraps);
   int beg = 0, end = 2*100000, mid;
   int ans = -1;
   while(beg <= end){
