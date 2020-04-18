709303e8816dfe240a09f53c76d984e937320c6c
LucaDantas <luca100sdantas2@gmail.com>
2020-04-18 20:18:51 -0300

    Did the F from r628 div2 cf

--- codeforces/virtual2/f.cpp
+++ codeforces/virtual2/f.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 
 #define pb push_back
+#define mp make_pair
 #define endl '\n'
 #define ll long long
 #define f first
@@ -15,15 +16,66 @@ using namespace std;
 
 const int maxn = 1e5 + 100;
 
-vector<int> g[maxn];
+vector<pair<int, bool>> g[maxn];
+bitset<maxn> visited;
+int level[maxn], pai[maxn], sq;
+pair<int, int> ciclo = {-1,-1};
+set<int> blocked;
+vector<int> pick;
+
+void dfs(int u=0, int l=0, int p=-1){
+	visited.set(u);
+	level[u] = l;
+	pai[u]=p;
+	int maxback=0;
+	for(auto &v : g[u]){
+		if(visited.test(v.f)){
+			maxback = max(maxback, level[u] - level[v.f]);
+		}
+		else{
+			v.s = true;
+			dfs(v.f, l+1, u);
+		}
+	}
+	if(maxback >= sq-1){
+		ciclo = {u, maxback+1};
+		return;
+	}
+	else if(blocked.find(u) == blocked.end() && (int)pick.size() < sq){
+		pick.pb(u);
+		for(auto v : g[u]){
+			blocked.insert(v.f);
+		}
+		if((int)pick.size() == sq) return;
+	}
+}
 
 int main(){
 	FAST;
 	int n, m;
 	cin >> n >> m;
-	for(int i = 1, a, b; i < m; i++){
+	sq = sqrt(n);
+	if(sq*sq != n) sq++;
+	for(int i = 0, a, b; i < m; i++){
 		cin >> a >> b;
-		g[a].pb(b);
-		g[b].pb(a);
+		a--,b--;
+		g[a].pb(mp(b,false));
+		g[b].pb(mp(a,false));
+	}
+	dfs();
+	if(ciclo != mp(-1,-1)){
+		int size = ciclo.s, u = ciclo.f;
+		cout << 2 << endl << size << endl;
+		while(size--){
+			cout << u+1 << ' ';
+			u = pai[u];
+		}
+		cout << endl;
+	}
+	else{
+		cout << 1 << endl;
+		for(auto it : pick)
+			cout << it+1 << ' ';
+		cout << endl;
 	}
 }
