8ce0983bdb21ebd37928373ad00c5c2295c56da6
LucaDantas <luca100sdantas2@gmail.com>
2020-04-20 21:11:30 -0300

    Updated the template

--- template.cpp
+++ template.cpp
@@ -14,6 +14,8 @@ typedef vector<int> vi;
 #define sz(a) ((int)(a).size())
 #define rep(i,a,b) for(int i=(a); i<(b); i++)
 #define dec(i,n,a) for(int i=(n); i>=(a); i--)
+#define read(v, a, b) for(int i=(a); i<(b); i++) cin>>v[i]
+#define printv(v, a, b) for(int i=(a); i < (b); i++) cout << v[i] << ' '
 #define clr(a,v) memset(a, v, sizeof(a))
 #define all(a) (a).begin(),(a).end()
 #define FAST cin.tie(0), cout.tie(0), ios::sync_with_stdio(0)
