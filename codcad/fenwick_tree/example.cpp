06ff02bfc9d92f59734ed526541be181a2d941d6
LucaDantas <luca100sdantas2@gmail.com>
2020-04-20 21:11:05 -0300

    Did a few BIT problems

--- codcad/fenwick_tree/example.cpp
+++ codcad/fenwick_tree/example.cpp
@@ -1,26 +1,31 @@
 #include<bits/stdc++.h>
 using namespace std;
 #define MAXN 100010
+#define read(v, a, b) for(int i=(a); i<(b); i++) cin>>v[i]
+#define printv(v, a, b) for(int i=(a); i < (b); i++) cout << v[i] << ' '
 
-tree[MAXN];
+int bit[MAXN], a[MAXN];
 int n;
 
-void update(int ind, int num){
-	while(ind <= n){
-		tree[x] += num;
-		ind += ind&-ind;
-	}
+void update(int x, int v){
+	for (; x <= n; x += (x&-x))
+        bit[x] += v;
 }
 
-int sum(int x){
-	int s = 0;
-	while(x!=0){
-		s += tree[x];
-		x-= x&-x;
-	}
-	return s;
+int query(int x){
+	int ans = 0;
+	for (; x > 0; x -= (x&-x))
+        ans += bit[x];
+	return ans;
 }
 
 int main(){
 	cin >> n;
+	read(a, 1, n+1);
+	for(int i = 1; i <= n; i++){
+		update(i, a[i]);
+	}
+	for(int i = 1; i <= n; i++)
+		cout << query(i) << ' ';
+	cout << endl;
 }
