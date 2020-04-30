480b52acbbf82685cd3d8598e85e3076618bb327
LucaDantas <luca100sdantas2@gmail.com>
2020-04-29 21:23:03 -0300

    Updated the template

--- template.cpp
+++ template.cpp
@@ -13,13 +13,11 @@ typedef vector<int> vi;
 #define s second
 #define sz(a) ((int)(a).size())
 #define rep(i,a,b) for(int i=(a); i<(b); i++)
-#define dec(i,n,a) for(int i=(n); i>=(a); i--)
 #define read(v, a, b) for(int i=(a); i<(b); i++) cin>>v[i]
 #define printv(v, a, b) for(int i=(a); i < (b); i++) cout << v[i] << ' '
 #define clr(a,v) memset(a, v, sizeof(a))
 #define all(a) (a).begin(),(a).end()
 #define FAST cin.tie(0), cout.tie(0), ios::sync_with_stdio(0)
-#define max3(x, y, z) max(x, max(y, z))
 #define db(x) cerr << #x << " == " << x << endl
 #define _ << " " <<
 
