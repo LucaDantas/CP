16596e3492ff3ed8ee2ad0ae50c704d7e260fc71
LucaDantas <luca100sdantas2@gmail.com>
2020-05-01 18:08:26 -0300

    Added p from r631 CF

--- codeforces/r631/b.cpp
+++ codeforces/r631/b.cpp
@@ -1,66 +1,54 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-#define ll long long
-#define pii pair<int, int>
-#define piii pair<int, pii>
-#define MAXN 1e5 + 100
-#define INF 1e9 + 100
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
 
-vector<int> a;
-set<int> ans;
+#define pb push_back
+#define mp make_pair
+#define endl '\n'
+#define sz(a) ((int)(a).size())
+#define rep(i,a,b) for(int i=(a); i<(b); i++)
+#define read(v, a, b) for(int i=(a); i<(b); i++) cin>>v[i]
+#define printv(v, a, b) for(int i=(a); i < (b); i++) cout << v[i] << ' '
+#define clr(a,v) memset(a, v, sizeof(a))
+#define all(a) (a).begin(),(a).end()
+#define FAST cin.tie(0), cout.tie(0), ios::sync_with_stdio(0)
+#define db(x) cerr << #x << " == " << x << endl
+#define _ << " " <<
 
-bool check(int begin){
-    set<int> seen;
-    int m = 0, num = 0;
-    while(++begin < a.size()){
-        m = max(m, a[begin]);
-        num++;
-        if(seen.find(a[begin]) != seen.end()) return false;
-        seen.insert(a[begin]);
-    }
-    if(num == m) return true;
-    return false;
-}
+const int maxn = 2e5 + 100;
 
-void find(int begin){
-    set<int> seen;
-    int m = begin, first = begin;
-    while(++begin < a.size()){
-        if(a[begin] <= first || seen.find(a[begin]) != seen.end()){
-            break;
-        }
-        m = max(m, a[begin]);
-        if(begin == m){
-            if(check(begin)){
-                ans.insert(begin);
-            } 
-            find(begin);
-        }
-        seen.insert(a[begin]);
-    }
-}
-
-void solve(int n){
-    a.resize(n+1);
-    int mx = 0, num = 0;
-    for(int i = 1; i <= n; i++){
-        scanf("%d", &a[i]);
-    }
-    find(0);
+bool check(const vi &v, int pos){
+	set<int> s;
+	for(int i = 0; i < pos; i++){
+		if(v[i] > pos) return false;
+		s.insert(v[i]);
+	}
+	if(sz(s) != pos) return false;
+	s.clear();
+	for(int i = pos; i < sz(v); i++){
+		if(v[i] > sz(v)-pos) return false;
+		s.insert(v[i]);
+	}
+	if(sz(s) != sz(v) - pos) return false;
+	return true; 
 }
 
 int main(){
-    int t;
-    scanf("%d", &t);
-    for(int i = 0, n; i < t; i++){
-        scanf("%d", &n);
-        solve(n);
-        printf("%d\n", (int)ans.size());
-        for(auto it : ans){
-            printf("%d %d\n", it, n-it);
-        }
-        ans.clear();
-    }
-    return 0;
+	FAST;
+	int t; cin >> t;
+	while(t--){
+		int n; cin >> n;
+		vi a(n);
+		read(a, 0, n);
+		int maior = *max_element(all(a));
+		bool a1 = check(a, maior);
+		bool a2 = maior * 2 != n && check(a, n-maior);
+		cout << a1 + a2 << endl;
+		if(a1) cout << maior _ n-maior << endl;
+		if(a2) cout << n-maior _ maior << endl;
+	}
 }
