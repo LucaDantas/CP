68f90402678487e5d97ef2f2e41f72d9fc31d0fa
LucaDantas <luca100sdantas2@gmail.com>
2020-05-03 21:37:20 -0300

    Added the first solution to cachecol - Recurrence matrix exponentiation

---
+++ obi/2013/cachecol/matrix_expo_recurrence.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef vector<vector<ll>> matrix;
+
+#define endl '\n'
+#define sz(a) ((int)(a).size())
+#define db(x) cerr << #x << " == " << x << endl
+#define _ << " " <<
+const ll MOD = 1e9 + 7;
+
+//Solve for the n-th item item of the recurrence
+//f(i) = 5f(i-1) - 2f(i-2)
+
+//vector[i][0] represents the first item of the row
+//vector[0][i] represents the top item of the column
+
+matrix operator *(matrix a, matrix b) {
+	int n = (int)a.size();
+	int m = (int)b.size();
+	int p = (int)b[0].size();
+	matrix c(n, vector<ll>(p));
+	vector<ll> col(m);
+	for (int j = 0; j < p; j++) {
+		for (int k = 0; k < m; k++)
+			col[k] = b[k][j]%MOD;
+		for (int i = 0; i < n; i++) {
+			ll s = 0;
+			for (int k = 0; k < m; k++)
+				s = ((s%MOD+((a[i][k]%MOD) * (col[k]%MOD))%MOD)%MOD + MOD)%MOD;
+			c[i][j] = s;
+		}
+	}
+	return c;
+}
+
+matrix power(matrix A, ll p){
+	if(p==1)
+		return A;
+	if(p%2)
+		return (A * (power(A, p-1)));
+	matrix X = power(A, (p>>1));
+	return X*X;
+}
+
+
+
+int main(){
+	ll n; cin >> n;
+	if(n==1) {cout << 12 << endl; return 0;}
+	if(n==2) {cout << 54 << endl; return 0;}
+	matrix base = {{54}, {12}};
+	matrix top = {{5, -2},{1, 0}};
+	matrix ans = power(top, n-2)*base;
+	cout << ans[0][0] << endl;
+}
