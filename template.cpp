397d567fcdc6701c3319f72b6b1d22dbfa7c3ade
LucaDantas <luca100sdantas2@gmail.com>
2020-05-06 20:52:47 -0300

    Updated template

--- template.cpp
+++ template.cpp
@@ -2,19 +2,19 @@
 using namespace std;
 
 typedef long long ll;
-typedef long double ld;
 typedef pair<int, int> pii;
+typedef pair<int, pii> piii;
 typedef vector<int> vi;
+typedef vector<ll> vll;
 
 #define pb push_back
 #define mp make_pair
 #define endl '\n'
-#define f first
-#define s second
+#define ff first
+#define ss second
 #define sz(a) ((int)(a).size())
 #define rep(i,a,b) for(int i=(a); i<(b); i++)
 #define read(v, a, b) for(int i=(a); i<(b); i++) cin>>v[i]
-#define printv(v, a, b) for(int i=(a); i < (b); i++) cout << v[i] << ' '
 #define clr(a,v) memset(a, v, sizeof(a))
 #define all(a) (a).begin(),(a).end()
 #define FAST cin.tie(0), cout.tie(0), ios::sync_with_stdio(0)
