42d2228bc258de597890fff8275528644090f370
LucaDantas <luca100sdantas2@gmail.com>
2020-05-27 13:38:58 -0300

    Added A and B from r645 div2

---
+++ codeforces/r645/b.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<int, pii> piii;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define sz(a) ((int)(a).size())
+#define rep(i,a,b) for(int i=(a); i<(b); i++)
+#define trav(it, a) for(auto& it : a)
+#define allin(it, a) for(auto it : a)
+#define read(v, a, b) for(int i=(a); i<(b); i++) scanf("%d", &v[i]);
+#define clr(a,v) memset(a, v, sizeof(a))
+#define all(a) (a).begin(),(a).end()
+#define FAST cin.tie(0), cout.tie(0), ios::sync_with_stdio(0)
+#define db(x) cerr << #x << " == " << x << endl
+
+const int maxn = 1e5 + 100;
+
+int main(){
+	int T; scanf("%d", &T);
+	while(T--) {
+		 int n; scanf("%d", &n);
+		 vi a(n);
+		 read(a,0,n);
+		 sort(all(a));
+		 int count = 1, sure = 1;
+		 rep(i,0,n) {
+			 int last = a[i];
+			 if(last <= count)
+				sure = count+1;
+			count++;
+		 }
+		 printf("%d\n", sure);
+	}
+}
