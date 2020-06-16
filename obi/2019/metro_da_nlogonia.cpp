7f8309834619f35137ec5c4a982c6e375f7a9c9e
LucaDantas <luca100sdantas2@gmail.com>
2020-06-16 14:02:51 -0300

    Added metro da nlogonia from OBI2019 Stage 3
    All we have to do is to find the center of two trees and print them, but if there are two we must find which one is further away from 1

--- obi/2019/metro_da_nlogonia.cpp
+++ obi/2019/metro_da_nlogonia.cpp
@@ -1,47 +1,94 @@
-#include<iostream>
-#include<pair>
-#include<unordered_map>
-#include<vector>
-
+#include <bits/stdc++.h>
 using namespace std;
 
-pair<int, vertex> maxp;
-maxp.first = 0;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<int, pii> piii;
+typedef vector<int> vi;
+typedef vector<ll> vll;
 
-struct vertex{
-    int name;
-    vector<vertex> adj;
-    bool seen = false;
-    int value;
-};
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define sz(a) ((int)(a).size())
+#define rep(i,a,b) for(int i=(a); i<(b); i++)
+#define trav(it, a) for(auto& it : a)
+#define allin(it, a) for(auto it : a)
+#define read(v, a, b) for(int i=(a); i<(b); i++) scanf("%d", &v[i]);
+#define clr(a,v) memset(a, v, sizeof(a))
+#define all(a) (a).begin(),(a).end()
+#define FAST cin.tie(0), cout.tie(0), ios::sync_with_stdio(0)
+#define db(x) cerr << #x << " == " << x << endl
 
-vector<vertex> vertices;
+const int maxn = 1e5 + 100;
 
-int path(vertex v){
-    if(v.adj.size() == 1 && v.adj[0].seen == true){ vertices[v.name].seen = true; vertices[v.name].value = 1; return 1; }
-    int max1 = 0, max2 = 0;
-    for(vertex k : v.adj){
-        if(vertices[k.name].seen) continue;
-        tam = path(k);
-        if(tam > max1) {max2 = max1; max1 = tam;}
-        else if(tam > max2) max2 = tam;
-    }
-    if(max1 + max2 + 1 > maxp.first) {maxp.first = max1 + max2 + 1; maxp.second = v;}
-    vertices[v.name].seen = true;
-    vertices[v.name].value = max1 + 1;
-    return max1 + 1;
+vi g[maxn][2];
+
+int grau[maxn][2], tam[2], dist[maxn][2];
+
+void dfs(int u, int pai, int k) {
+	for(auto v : g[u][k]) {
+		if(v == pai) continue;
+		dist[v][k] = dist[u][k]+1;
+		dfs(v,u,k);
+	}
+}
+
+int center(int k) {
+	queue<int> leafs;
+
+	for(int i = 1; i <= tam[k]; i++) {
+		if(grau[i][k] == 1)
+			leafs.push(i);
+	}
+
+	int used = 0;
+	while(tam[k] - used > 2) {
+		int n = sz(leafs);
+		used += n;
+		while(n--) {
+			int u = leafs.front();
+			leafs.pop();
+			grau[u][k]--; // Makes its degree 0 not to be counted again
+			for(auto v : g[u][k]) {
+				grau[v][k]--;
+				if(grau[v][k] == 1)
+					leafs.push(v);
+			}
+		}
+	}
+
+	dist[1][k] = 0;
+	dfs(1,-1,k);
+	
+	if(sz(leafs) == 1)
+		return leafs.front();
+	else {
+		int u1 = leafs.front();
+		leafs.pop();
+		int u2 = leafs.front();
+		leafs.pop();
+		if(dist[u1][k] > dist[u2][k])
+			return u1;
+		else
+			return u2;
+	}
 }
 
 int main(){
-    int n, m;
-    cin >> n >> m;
-    vertices.resize(n+10);
-    for(int i = 1; i < n; i++){
-        int a, b;
-        cin >> a >> b;
-        vertices[a].adj.push_back(vertices[b]);
-        vertices[b].adj.push_back(vertices[a]);
-    }
-    path(vertices[1]);
-    //Já fiz o de achar o maior caminho com um vértice desse caminho, agora basta achar o vértice do meio desse caminho
+	scanf("%d %d", &tam[0], &tam[1]);
+
+	for(int k = 0; k < 2; k++) {
+		for(int i = 1; i < tam[k]; i++) {
+			int u, v;
+			scanf("%d %d", &u, &v);
+			g[u][k].pb(v);
+			g[v][k].pb(u);
+			grau[u][k]++;
+			grau[v][k]++;
+		}
+	}	
+
+	printf("%d %d\n", center(0), center(1));
 }
