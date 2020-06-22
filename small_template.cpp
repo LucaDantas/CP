656b35eba10058c200435f42c8bac48058568dbd
LucaDantas <luca100sdantas2@gmail.com>
2020-06-22 20:15:27 -0300

    Added smaller template for OBI (c++11), makefile and cf contest template

---
+++ small_template.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using pii = pair<int, int>;
+using vi = vector<int>;
+
+#ifdef MY_DEBUG_FLAG
+#define db(...) cerr << "DEBUG( " << #__VA_ARGS__ << " == " << __VA_ARGS__ << " )" << endl
+#else
+#define db(...)
+#endif // define(MY_DEBUG_FLAG)
+
+#define pb emplace_back
+#define ff first
+#define ss second
+#define sz(a) ((int)(a).size())
+#define rep(i,a,b) for(int i=(a); i<(b); i++)
+#define dec(i,n,a) for(int i=(n); i>=(a); i--)
+#define clr(a,v) memset(a, v, sizeof(a))
+#define all(a) (a).begin(),(a).end()
+
+constexpr int inf = 0x3f3f3f3f;
+constexpr int MAXN = 1e5 + 10;
+constexpr int LOGN = 20;
+constexpr int mod = 1000000007;
+
+int main() {
+	
+}
