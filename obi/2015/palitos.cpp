cc38580a6107207dc042417b4ecf632a0c729ce5
LucaDantas <luca100sdantas2@gmail.com>
2020-07-05 20:45:16 -0300

    Added palitos frm OBI2015

---
+++ obi/2015/palitos.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using pii = pair<int, int>;
+using vi = vector<int>;
+
+#ifdef MY_DEBUG_FLAG
+template<typename T> void debug(T a) { cerr << a << ' '; }
+template<typename T, typename U> void debug(pair<T, U> a) { cerr << a.first << ' ' << a.second << ' '; }
+template<typename T> void debug(vector<T> a) { for(auto it : a) debug(it);}
+template<typename T> void debug(set<T> a) { for(auto it : a) debug(it);}
+#define db(a) cerr << "DEBUG ( " << #a << " == "; debug(a); cerr << ")\n"
+#else
+#define db(...)
+#endif
+
+#define pb push_back
+#define eb emplace_back
+#define ff first
+#define ss second
+#define fast ios_base::sync_with_stdio(false), cout.tie(nullptr), cin.tie(nullptr)
+#define sz(a) ((int)(a).size())
+#define rep(i,a,b) for(int i=(a); i<(b); i++)
+#define dec(i,n,a) for(int i=(n); i>=(a); i--)
+#define clr(a,v) memset(a, v, sizeof(a))
+#define all(a) (a).begin(),(a).end()
+
+constexpr int inf = 0x3f3f3f3f;
+constexpr int MAXN = 1e5 + 10;
+constexpr int mod = 1000000007;
+
+int main() {
+	int n, m;
+	scanf("%d %d", &n, &m);
+	if(m*3 < n) {puts("0"); return 0; }
+	n--;
+	ll ans = 1ll*(n-1)*n / 2ll;
+	int prox = n-m;
+	if(prox >= 2)
+		ans -= 3ll*prox*(prox-1)/2;
+	prox -= m;
+	if(prox >= 2)
+		ans += 3ll*prox*(prox-1)/2;
+	printf("%lld\n", ans);
+}
