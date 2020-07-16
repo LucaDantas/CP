83d0852381f1acb2e6d7fd289a3e94b8fe74faa5
LucaDantas <luca100sdantas2@gmail.com>
2020-07-15 22:16:45 -0300

    Added a better version of template

--- template.cpp
+++ template.cpp
@@ -4,12 +4,15 @@ using ll = long long;
 using pii = pair<int, int>;
 using vi = vector<int>;
 
+template<typename T> ostream& operator<<(ostream &os, const vector<T> &v) { os << '['; string sep = ""; for (const auto &x : v) os << sep << x, sep = ", "; return os << ']'; }
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename A> ostream& operator<<(ostream &os, const set<A> &s) { os << '{'; string sep = ""; for (const auto &x : s) os << sep << x, sep = ", "; return os << '}'; }
+template<typename A, typename B> ostream& operator<<(ostream &os, const map<A, B> &m) { os << '{'; string sep = ""; for (const auto &x : m) os << sep << x.first << " -> " << x.second, sep = ", "; return os << '}'; }
+
 #ifdef MY_DEBUG_FLAG
-template<typename T> void debug(T a) { cerr << a << ' '; }
-template<typename T, typename U> void debug(pair<T, U> a) { cerr << a.first << ' ' << a.second << ' '; }
-template<typename T> void debug(vector<T> a) { for(auto it : a) debug(it);}
-template<typename T> void debug(set<T> a) { for(auto it : a) debug(it);}
-#define db(a) cerr << "DEBUG ( " << #a << " == "; debug(a); cerr << ")\n"
+void debug() { cerr << '\n'; }
+template<typename Ini, typename... Fim> void debug(Ini I, Fim... F) { cerr << " " << I; debug(F...); }
+#define db(...) cerr << "DEBUG (" << #__VA_ARGS__ << ") ==", debug(__VA_ARGS__)
 #else
 #define db(...)
 #endif
@@ -26,7 +29,7 @@ template<typename T> void debug(set<T> a) { for(auto it : a) debug(it);}
 #define all(a) (a).begin(),(a).end()
 
 constexpr int inf = 0x3f3f3f3f;
-constexpr int MAXN = 1e5 + 10;
+constexpr int maxn = 1e5 + 10;
 constexpr int mod = 1000000007;
 
 int main() {
