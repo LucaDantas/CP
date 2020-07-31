d346bea477dea6b122848add9b6535bc0575bdcf
LucaDantas <luca100sdantas2@gmail.com>
2020-07-30 21:39:23 -0300

    Added E from global round 1

---
+++ codeforces/GlobalRound/1/E.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using pii = pair<int, int>;
+using vi = vector<int>;
+
+template<typename T> ostream& operator<<(ostream &os, const vector<T> &v) { os << '['; string sep = ""; for (const auto &x : v) os << sep << x, sep = ", "; return os << ']'; }
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename A> ostream& operator<<(ostream &os, const set<A> &s) { os << '{'; string sep = ""; for (const auto &x : s) os << sep << x, sep = ", "; return os << '}'; }
+template<typename A, typename B> ostream& operator<<(ostream &os, const map<A, B> &m) { os << '{'; string sep = ""; for (const auto &x : m) os << sep << x.first << " -> " << x.second, sep = ", "; return os << '}'; }
+
+#ifdef MY_DEBUG_FLAG
+void debug() { cerr << '\n'; }
+template<typename Ini, typename... Fim> void debug(Ini I, Fim... F) { cerr << " " << I; debug(F...); }
+#define db(...) cerr << "DEBUG (" << #__VA_ARGS__ << ") ==", debug(__VA_ARGS__)
+#else
+#define db(...)
+#endif
+
+#define pb push_back
+#define eb emplace_back
+#define ff first
+#define ss second
+#define fast ios_base::sync_with_stdio(false), cout.tie(nullptr), cin.tie(nullptr)
+#define sz(a) ((int)(a).size())
+#define rep(i,a,b) for(int i=(a); i<(b); i++)
+#define dec(i,n,a) for(int i=(n); i>=(a); i--)
+#define clr(a,v) memset(a, v, sizeof(a))
+#define all(a) (a).begin(),(a).end()
+
+constexpr int inf = 0x3f3f3f3f;
+constexpr int maxn = 1e5 + 10;
+constexpr int mod = 1000000007;
+
+int main() {
+	int n; scanf("%d", &n);
+	vi a(n), b(n);
+	rep(i,0,n) scanf("%d", &a[i]);
+	rep(i,0,n) scanf("%d", &b[i]);
+	bool ok = 1;
+	if(a[0] != b[0]) ok = 0;
+	vi s, d;
+	rep(i,1,n) s.eb(a[i] - a[i-1]);
+	rep(i,1,n) d.eb(b[i] - b[i-1]);
+	sort(all(s));
+	sort(all(d));
+	rep(i,0,n-1) if(s[i] != d[i]) ok = 0;
+	puts(ok?"Yes":"No");
+}
