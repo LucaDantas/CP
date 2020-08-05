b0dfa7122ba003a67bbd442aadf7c02af7d1fa23
LucaDantas <luca100sdantas2@gmail.com>
2020-08-05 12:18:40 -0300

    Added D from CF Div2 657

---
+++ codeforces/Div2/657/D.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using pii = pair<int, int>;
+using vi = vector<int>;
+
+template<typename T> ostream& operator<<(ostream &os, const vector<T> &v) { os << '['; string sep = ""; for (const auto &x : v) os << sep << x, sep = ", "; return os << ']'; }
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename A> ostream& operator<<(ostream &os, const set<A> &s) { os << '{'; string sep = ""; for (const auto &x : s) os << sep << x, sep = ", "; return os << '}'; }
+template<typename A, typename B> ostream& operator<<(ostream &os, const map<A, B> &m) { os << '{'; string sep = ""; for (const auto &x : m) os << sep << x.first << " -> " << x.second, sep = ", "; return os << '}'; }
+
+#ifdef MY_DEBUG_FLAG
+void debug() { cerr << '\n'; }
+template<typename Ini, typename... Fim> void debug(Ini I, Fim... F) { cerr << " " << I; debug(F...); }
+#define db(...) cerr << "DEBUG (" << #__VA_ARGS__ << ") ==", debug(__VA_ARGS__)
+#else
+#define db(...)
+#endif
+
+#define pb push_back
+#define eb emplace_back
+#define ff first
+#define ss second
+#define fast ios_base::sync_with_stdio(false), cout.tie(nullptr), cin.tie(nullptr)
+#define sz(a) ((int)(a).size())
+#define rep(i,a,b) for(int i=(a); i<(b); i++)
+#define dec(i,n,a) for(int i=(n); i>=(a); i--)
+#define clr(a,v) memset(a, v, sizeof(a))
+#define all(a) (a).begin(),(a).end()
+
+constexpr int inf = 0x3f3f3f3f;
+constexpr int maxn = 1e5 + 10;
+constexpr int mod = 1000000007;
+
+// we can completely ignore the hours because we simply reflect all on one hour
+// and we can do the same with the two halves, because they'll reflect one on
+// the other, so we take everything mod m/2 as soon as we read them
+
+int main() {
+	int n, h, m, k; scanf("%d %d %d %d", &n, &h, &m, &k);
+	vector<pii> freight(n); // stores the minute and the index
+	rep(i,0,n) {
+		int trash, minute; scanf("%d %d", &trash, &minute);
+		freight[i] = {minute % (m/2), i+1};
+	}
+	sort(all(freight));
+	rep(i,0,n) {
+		freight.eb(freight[i].ff+(m/2), freight[i].ss);
+	}
+	
+	int r = 1, here = 0, ans = inf, tp = 0, id = -1;
+	for(int l = 0; l < n; l++) {
+		for(; freight[r].ff - freight[l].ff < k; r++)
+			here++;
+		if(here < ans) ans = here, tp = (freight[l].ff+k)%(m/2), id = l;
+		here--;
+	}
+	printf("%d %d\n", ans, tp);
+	for(int i = id+1; freight[i].ff - freight[id].ff < k; i++)
+		printf("%d ", freight[i].ss);
+	printf("\n");
+}
