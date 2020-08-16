b41ceb3262c14f9684008ff8641525172cc155c7
LucaDantas <luca100sdantas2@gmail.com>
2020-08-16 11:28:56 -0300

    Updated template

--- template.cpp
+++ template.cpp
@@ -10,9 +10,9 @@ template<typename A> ostream& operator<<(ostream &os, const set<A> &s) { os << '
 template<typename A, typename B> ostream& operator<<(ostream &os, const map<A, B> &m) { os << '{'; string sep = ""; for (const auto &x : m) os << sep << x.first << " -> " << x.second, sep = ", "; return os << '}'; }
 
 #ifdef MY_DEBUG_FLAG
-void debug() { cerr << '\n'; }
-template<typename Ini, typename... Fim> void debug(Ini I, Fim... F) { cerr << " " << I; debug(F...); }
-#define db(...) cerr << "DEBUG (" << #__VA_ARGS__ << ") ==", debug(__VA_ARGS__)
+void debug() { cerr << endl; }
+template<typename Ini, typename... Fim> void debug(Ini I, Fim... F) { cerr << I; if(sizeof...(F)) cerr << ", "; debug(F...);}
+#define db(...) cerr << "DEBUG (" << #__VA_ARGS__ << ") == ", debug(__VA_ARGS__)
 #else
 #define db(...)
 #endif
