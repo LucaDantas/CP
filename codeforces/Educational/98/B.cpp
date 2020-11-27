06ff2a922c5376e06cbc43a9156e4e3f4589f22f
LucaDantas <luca100sdantas2@gmail.com>
2020-11-27 15:38:05 -0300

    Added CF Edu98 A..E

---
+++ codeforces/Educational/98/B.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using pii = pair<int, int>;
+using vi = vector<int>;
+
+template<typename T> ostream& operator<<(ostream &os, const vector<T> &v) { os << '['; string sep = ""; for (const auto &x : v) os << sep << x, sep = ", "; return os << ']'; }
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename A> ostream& operator<<(ostream &os, const set<A> &s) { os << '{'; string sep = ""; for (const auto &x : s) os << sep << x, sep = ", "; return os << '}'; }
+template<typename A, typename B> ostream& operator<<(ostream &os, const map<A, B> &m) { os << '{'; string sep = ""; for (const auto &x : m) os << sep << x.first << " -> " << x.second, sep = ", "; return os << '}'; }
+
+#ifdef MY_DEBUG_FLAG
+void debug() { cerr << endl; }
+template<typename Ini, typename... Fim> void debug(Ini I, Fim... F) { cerr << I; if(sizeof...(F)) cerr << ", "; debug(F...);}
+#define db(...) cerr << "DEBUG (" << #__VA_ARGS__ << ") == ", debug(__VA_ARGS__)
+#else
+#define db(...)
+#endif
+
+#define pb push_back
+#define eb emplace_back
+#define ff first
+#define ss second
+#define fast ios_base::sync_with_stdio(false), cout.tie(nullptr), cin.tie(nullptr)
+#define sz(a) ((int)(a).size())
+#define rep(i,a,b) for(int i=(a); i<(b); i++)
+#define dec(i,n,a) for(int i=(n); i>=(a); i--)
+#define clr(a,v) memset(a, v, sizeof(a))
+#define all(a) (a).begin(),(a).end()
+
+constexpr int inf = 0x3f3f3f3f;
+constexpr int maxn = 1e5 + 10;
+constexpr int mod = 1000000007;
+
+int main() {
+	int t; scanf("%d", &t);
+	while(t--) {
+		int n; scanf("%d", &n);
+		vector<int> a(n);
+		int mx = 0, mn = inf;
+		ll sum = 0;
+		for(int& x : a) {
+			scanf("%d", &x);
+			mx = max(x, mx);
+			mn = max(x, mn);
+			sum += x;
+		}
+		ll ans = -1;
+		int l = 0, r = 1e9;
+		while(l <= r) {
+			int k = (l+r) >> 1;
+			if(1ll*(mx+k)*(n-1) - sum >= 0)
+				ans = 1ll*(mx+k)*(n-1) - sum, r = k-1;
+			else l = k+1;
+		}
+		assert(ans != -1);
+		printf("%lld\n", ans);
+	}
+}
---
+++ codeforces/Educational/98/E.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using pii = pair<int, int>;
+using vi = vector<int>;
+
+template<typename T> ostream& operator<<(ostream &os, const vector<T> &v) { os << '['; string sep = ""; for (const auto &x : v) os << sep << x, sep = ", "; return os << ']'; }
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename A> ostream& operator<<(ostream &os, const set<A> &s) { os << '{'; string sep = ""; for (const auto &x : s) os << sep << x, sep = ", "; return os << '}'; }
+template<typename A, typename B> ostream& operator<<(ostream &os, const map<A, B> &m) { os << '{'; string sep = ""; for (const auto &x : m) os << sep << x.first << " -> " << x.second, sep = ", "; return os << '}'; }
+
+#ifdef MY_DEBUG_FLAG
+void debug() { cerr << endl; }
+template<typename Ini, typename... Fim> void debug(Ini I, Fim... F) { cerr << I; if(sizeof...(F)) cerr << ", "; debug(F...);}
+#define db(...) cerr << "DEBUG (" << #__VA_ARGS__ << ") == ", debug(__VA_ARGS__)
+#else
+#define db(...)
+#endif
+
+#define pb push_back
+#define eb emplace_back
+#define ff first
+#define ss second
+#define fast ios_base::sync_with_stdio(false), cout.tie(nullptr), cin.tie(nullptr)
+#define sz(a) ((int)(a).size())
+#define rep(i,a,b) for(int i=(a); i<(b); i++)
+#define dec(i,n,a) for(int i=(n); i>=(a); i--)
+#define clr(a,v) memset(a, v, sizeof(a))
+#define all(a) (a).begin(),(a).end()
+
+constexpr int inf = 0x3f3f3f3f;
+constexpr int maxn = 1e5 + 10;
+constexpr int mod = 1000000007;
+
+
+int main() {
+	int n, m, k; scanf("%d %d %d", &n, &m, &k);
+	vector<pii> events;
+	for(int i = 0; i < m; i++) {
+		int l, r; scanf("%d %d", &l, &r);
+		events.pb({l, r});
+	}
+	sort(all(events), [](const pii& a, const pii& b) {
+		return (a.ff+a.ss)/2 < (b.ff+b.ss)/2;
+	});
+	vector<int> suf(m+1);
+	for(int i = 1; i <= n-k+1; i++) {
+		for(int j = m-1, cur = 0; j >= 0; j--) {
+			cur += max(0, min(i+k-1, events[j].ss) - max(i, events[j].ff) + 1);
+			suf[j] = max(suf[j], cur);
+		}
+	}
+	int ans = 0;
+	for(int i = 1; i <= n-k+1; i++) {
+		for(int j = 0, cur = 0; j < m; j++) {
+			cur += max(0, min(i+k-1, events[j].ss) - max(i, events[j].ff) + 1);
+			ans = max(ans, cur + suf[j+1]);
+		}
+	}
+	printf("%d\n", ans);
+}
