67f105ec0e351e037115973b31873c733cae3917
LucaDantas <luca100sdantas2@gmail.com>
2021-02-08 12:34:44 -0300

    Added CF 700Div2 A..D2

---
+++ codeforces/Div2/700/D2.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using pii = pair<int, int>;
+using vi = vector<int>;
+
+template<typename T> ostream& operator<<(ostream &os, const vector<T> &v) { os << '['; string sep = ""; for (const auto &x : v) os << sep << x, sep = ", "; return os << ']'; }
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename A> ostream& operator<<(ostream &os, const set<A> &s) { os << '{'; string sep = ""; for (const auto &x : s) os << sep << x, sep = ", "; return os << '}'; }
+template<typename A, typename B> ostream& operator<<(ostream &os, const map<A, B> &m) { os << '{'; string sep = ""; for (const auto &x : m) os << sep << x.first << " -> " << x.second, sep = ", "; return os << '}'; }
+
+#ifdef MY_DEBUG_FLAG
+void debug() { cerr << endl; }
+template<typename Ini, typename... Fim> void debug(Ini I, Fim... F) { cerr << I; if(sizeof...(F)) cerr << ", "; debug(F...);}
+#define db(...) cerr << "DEBUG (" << #__VA_ARGS__ << ") == ", debug(__VA_ARGS__)
+#else
+#define db(...)
+#endif
+
+#define pb push_back
+#define eb emplace_back
+#define ff first
+#define ss second
+#define fast ios_base::sync_with_stdio(false), cout.tie(nullptr), cin.tie(nullptr)
+#define sz(a) ((int)(a).size())
+#define rep(i,a,b) for(int i=(a); i<(b); i++)
+#define dec(i,n,a) for(int i=(n); i>=(a); i--)
+#define clr(a,v) memset(a, v, sizeof(a))
+#define all(a) (a).begin(),(a).end()
+
+constexpr int inf = 0x3f3f3f3f;
+constexpr int maxn = 2e5 + 10;
+constexpr int mod = 1000000007;
+
+int dp[maxn], last_grp[maxn];
+
+int main() {
+	int n; scanf("%d", &n);
+	vector<int> a;
+	for(int i = 0, v, last = inf; i < n; i++) {
+		scanf("%d", &v);
+		if(v != last) a.pb(v);
+		last = v;
+	}
+	n = sz(a);
+	for(int i = n-1; i >= 0; i--) {
+		dp[i] = dp[i+1] + 1;
+		int ult = last_grp[a[i]];
+		if(ult) {
+			dp[i] = min(dp[i], dp[ult] + (ult - i - 1));
+			dp[i] = min(dp[i], (ult - i - 1) + (dp[ult-1] - 1));
+		}
+		last_grp[a[i]] = i;
+	}
+	printf("%d\n", dp[0]);
+}
